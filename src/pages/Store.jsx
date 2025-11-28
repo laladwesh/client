dbfc145c0a8054d190888978245aaf70db079d64
laladwesh <guptaavinash302@gmail.com>
2025-11-29 03:57:52 +0530

    store page done

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProductDisplay from '../components/New';
 
 const Store = () => {
   return (
-    <div>
-      <h1>Store - Coming Soon</h1>
-      <p>Our collection will be available soon!</p>
+    <div className=''>
+      <h1 className='font-bdogrotesk-black text-black text-9xl px-6 py-28'>Store</h1>
+    <ProductDisplay isStore={true} />
     </div>
   );
 };
