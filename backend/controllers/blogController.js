0d52b90998e0f86fb07a21ee19622a4c49758c02
laladwesh <guptaavinash302@gmail.com>
2025-12-17 19:42:26 +0530

    Commit backend/controllers/blogController.js

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,57 @@
+const asyncHandler = require('express-async-handler');
+const Blog = require('../models/Blog');
+
+// GET /api/blogs
+const getBlogs = asyncHandler(async (req, res) => {
+  const blogs = await Blog.find({}).sort({ createdAt: -1 });
+  res.json(blogs);
+});
+
+// GET /api/blogs/:id
+const getBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    res.status(404);
+    throw new Error('Blog not found');
+  }
+  res.json(blog);
+});
+
+// POST /api/blogs
+const createBlog = asyncHandler(async (req, res) => {
+  const data = req.body;
+  if (!data.title) {
+    res.status(400);
+    throw new Error('Title is required');
+  }
+  if (!data.slug) {
+    data.slug = data.title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+  }
+  const blog = await Blog.create(data);
+  res.status(201).json(blog);
+});
+
+// PUT /api/blogs/:id
+const updateBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    res.status(404);
+    throw new Error('Blog not found');
+  }
+  Object.assign(blog, req.body, { updatedAt: Date.now() });
+  await blog.save();
+  res.json(blog);
+});
+
+// DELETE /api/blogs/:id
+const deleteBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    res.status(404);
+    throw new Error('Blog not found');
+  }
+  await blog.remove();
+  res.json({ message: 'Blog removed' });
+});
+
+module.exports = { getBlogs, getBlog, createBlog, updateBlog, deleteBlog };
