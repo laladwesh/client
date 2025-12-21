635e0d30725b9d9874c96841daf8c78dabac1da8
laladwesh <guptaavinash302@gmail.com>
2025-12-22 01:04:33 +0530

    Update dependencies : client/src/services/cartService.js

---
+++ client/src/services/cartService.js
@@ -0,0 +1,11 @@
+import api from './api';
+
+export const getCart = async () => {
+  const res = await api.get('/cart');
+  return res.data;
+};
+
+export const updateCart = async (cart) => {
+  const res = await api.put('/cart', { cart });
+  return res.data;
+};
