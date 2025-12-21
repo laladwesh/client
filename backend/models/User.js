9e17b2ab74b2e532e5c2a552860d5d41caea8b3b
laladwesh <guptaavinash302@gmail.com>
2025-12-22 01:04:29 +0530

    Fix bugs : backend/models/User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,30 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   avatar: { type: String },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  cart: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      qty: { type: Number, default: 1 },
+      size: { type: String },
+      price: { type: Number },
+      addedAt: { type: Date, default: Date.now }
+    }
+  ],
+  addresses: [
+    {
+      label: { type: String },
+      name: { type: String },
+      line1: { type: String },
+      line2: { type: String },
+      city: { type: String },
+      state: { type: String },
+      country: { type: String },
+      postalCode: { type: String },
+      phone: { type: String },
+      isDefault: { type: Boolean, default: false },
+      createdAt: { type: Date, default: Date.now },
+    }
+  ],
   createdAt: { type: Date, default: Date.now },
 });
 
