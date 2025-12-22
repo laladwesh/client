021da71c813fa6099f5a3ed5b07d524b05ba36c9
laladwesh <guptaavinash302@gmail.com>
2025-12-22 13:08:36 +0530

    Code improvements : backend/models/User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true, unique: true },
+  googleId: { type: String, unique: true, sparse: true },
   name: { type: String },
   email: { type: String, required: true, unique: true },
   avatar: { type: String },
