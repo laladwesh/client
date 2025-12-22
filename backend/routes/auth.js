7e06c70a62d3e53857b47dddf5a59247f6be8f3d
laladwesh <guptaavinash302@gmail.com>
2025-12-22 13:08:37 +0530

    Code improvements : backend/routes/auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { googleAuth, googleRedirect, googleCallback } from '../controllers/authController.js';
+import { sendOtp, verifyOtp, createUserFromTemp } from '../controllers/authOtpController.js';
 
 const router = express.Router();
 
 router.get('/google', googleRedirect);
 router.get('/google/callback', googleCallback);
 router.post('/google', googleAuth);
+router.post('/otp/send', sendOtp);
+router.post('/otp/verify', verifyOtp);
+router.post('/otp/create-user', createUserFromTemp);
 
 export default router;
