a610f33dd3bcaaa342c765047a1c87d8291f1589
laladwesh <guptaavinash302@gmail.com>
2026-01-03 13:37:20 +0530

    Improve documentation : backend/routes/orders.js

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -6,6 +6,11 @@ import {
   getOrderById,
   getOrders,
   updateOrderStatus,
+  updateOrderStage,
+  addDelhiveryWaybill,
+  trackOrder,
+  createDelhiveryShipment,
+  cancelDelhiveryShipment
 } from '../controllers/orderController.js';
 
 const router = express.Router();
@@ -13,9 +18,14 @@ const router = express.Router();
 router.post('/', protect, createOrder);
 router.get('/my', protect, getMyOrders);
 router.get('/:id', protect, getOrderById);
+router.get('/:id/track', protect, trackOrder);
 
 // admin
 router.get('/', protect, admin, getOrders);
 router.put('/:id/status', protect, admin, updateOrderStatus);
+router.put('/:id/stage', protect, admin, updateOrderStage);
+router.put('/:id/delhivery/waybill', protect, admin, addDelhiveryWaybill);
+router.post('/:id/delhivery/shipment', protect, admin, createDelhiveryShipment);
+router.delete('/:id/delhivery/cancel', protect, admin, cancelDelhiveryShipment);
 
 export default router;
